d57a28742a3ecdb8fc84234611b33def5108179a
loose <Oliver.Loose@adesso.de>
2020-03-28 16:52:41 +0100

    Adds simple cursor.

---
+++ Assets/Demo/Scripts/Indicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Indicator : MonoBehaviour {
+    [SerializeField]
+    private GameObject Cursor;
+
+    private ARRaycastManager rayManager;
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Start() {
+        rayManager = FindObjectOfType<ARRaycastManager>();
+
+        if (Cursor == null) {
+            Cursor = transform.GetChild(0).gameObject;
+        }
+    }
+
+    void Update() {
+        if(rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), 
+            hits,
+            TrackableType.PlaneEstimated | TrackableType.PlaneWithinPolygon)) {
+
+            transform.SetPositionAndRotation(hits[0].pose.position, hits[0].pose.rotation);
+        }
+    }
+}
